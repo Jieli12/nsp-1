d1bc969630fa6318cd6daa531137e5aec44ed3f7
Jie Li <lijie_12@hotmail.com>
2022-11-23 15:17:04 +0000

    format

--- testL2.R
+++ testL2.R
@@ -13,7 +13,7 @@ result_2 <- nsp(y, x, 100, 15 * thresh_kab(300, .1))
 result_1
 result_2
 
-# test for weight least square
+# test for weight least square: dyadic
 M <- 100
 n <- 300
 M <- min(M, (n - 1) * n / 2)
@@ -21,30 +21,46 @@ M <- min(M, (n - 1) * n / 2)
 ind0 <- grid_intervals_sorted(n, M)
 ads.array <- all_dyadic_scans_array(cbind(y,x))
 s <- ind0[1]
-e <- indo[2]
+e <- ind0[2]
 n <- e-s+1
 indices <- ((ads.array$shifts + 1) <= (n / 2))
-    
-    ads.array$res <- ads.array$res[1:300, , indices, drop = F]
-    
-    ads.array$shifts <- ads.array$shifts[indices]
-    
-    M <- min(M, (n - 1) * n / 2)
-    
-    ind <- grid_intervals_sorted(n, M)
-    
-    M <- dim(ind)[2]
-    
-    res <- matrix(0, M, 3)
-    
-    res[, 1:2] <- t(ind)
-    
-    zero.check <- TRUE
-    j <- 1
-    
-    while (zero.check && (j <= M)) {
-        res[j, 3] <- check_interval_array(res[j, 1:2], ads.array, thresh)
-        zero.check <- (res[j, 3] == 0)
-        j <- j + 1
-    }
-    
+
+ads.array$res <- ads.array$res[s:e, , indices, drop = F]
+
+ads.array$shifts <- ads.array$shifts[indices]
+
+dm <- dim(ads.array$res)
+
+f.con.rhs.core <- matrix(0, 0, dm[2])
+
+scales <- length(ads.array$shifts)
+
+for (i in 1:scales) {
+    f.con.rhs.current <- ads.array$res[1:(n - ads.array$shifts[i]), , i]
+    f.con.rhs.core <- rbind(f.con.rhs.core, f.con.rhs.current)
+}
+yd <- f.con.rhs.core[, 1]
+xd <- f.con.rhs.core[, -1]
+result_d <- lm(yd ~ xd - 1)
+summary(result_d)
+acf2(residuals(result_d))
+
+library(nlme)
+result_gls <- gls(yd ~ xd - 1)
+summary(result_gls)
+# weight least square
+freq <- rep(1,n)
+for (i in 2:scales) {
+    shift <- ads.array$shifts[i]
+    freq <- freq + c(1:shift,rep(2^(i-1),n-2*shift),shift:1) 
+}
+# freq <- freq/sum(freq)
+y_rep <- rep(y[1:22],freq)
+x_rep1 <- rep(x[1:22,1],freq)
+x_rep2 <- rep(x[1:22,2],freq)
+x_rep <- cbind(x_rep1,x_rep2)
+result_wt <- lm(y_rep ~ x_rep-1)
+summary(result_wt)
+
+
+
